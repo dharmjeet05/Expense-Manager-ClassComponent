5df2c7c022089ebe0bba4fd10368b7f54d3f5ffd
Dharmjeet Vaishnav <dharmajeet.vaishnav@yudiz.com>
2022-02-27 15:39:06 +0530

    UI: Card is all ready

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,28 @@
+/* eslint-disable react/jsx-key */
+// import PropTypes from "prop-types";
 import React, { Component } from "react";
+import "./Main.css";
 
 export class Main extends Component {
+  // static propTypes = {};
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <div>Main</div>;
+    return (
+      <div className="container">
+        <div className="main">
+          <div className="main__item">
+            <div className="main__item__top">
+              <h2>Title</h2>
+              <h5>Amount</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
